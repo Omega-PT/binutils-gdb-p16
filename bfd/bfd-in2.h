3a39d1984c71084f4e9a46458f578bcebd35df60
Omega-PT <filipepegado2004@outlook.pt>
2025-06-07 19:26:01 +0100

    Missing arch definition in an... auto generated file

--- bfd/bfd-in2.h
+++ bfd/bfd-in2.h
@@ -1842,6 +1842,8 @@ enum bfd_architecture
 #define bfd_mach_amdgcn_gfx1100 0x041
 #define bfd_mach_amdgcn_gfx1101 0x046
 #define bfd_mach_amdgcn_gfx1102 0x047
+  bfd_arch_p16,
+#define bfd_mach_p16 1
   bfd_arch_last
   };
 
